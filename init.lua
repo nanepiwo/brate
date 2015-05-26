9657c287e87a57d5f9915f7690abc87137f3d229
nanepiwo <spamdumpingstation@gmail.com>
2015-05-26 08:54:56 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
----HI
+minetest.register_node("brate:rate", {
+  description = "Building Rater",
+  tiles = {"brate_rate.png", "brate_rate_side.png", "brate_rate_side.png",
+  "brate_rate_side.png", "brate_rate_side.png", "brate_rate_side.png" },
+  paramtype2 = "facedir",
+	groups = {cracky=2},
+	is_ground_content = false,
+	on_rightclick = function(pos, node, clicker)
+    brate.show_rater_formspec( clicker, pos );
+	end,
+	
